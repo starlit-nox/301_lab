72b50178ea4314f7afad9a6911ad4fdf4ca2e877
starlit-nox <kdkelly.1998@gmail.com>
2023-04-20 13:38:34 -0500

    Class 02 Lab Completed

--- lab01-app/src/App.js
+++ lab01-app/src/App.js
@@ -10,19 +10,8 @@ function App() {
       <Header/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <Main/>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
       </header>
+      <Main/>
       <Footer/>
     </div>
   );
